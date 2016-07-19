f12d3a3fc3262950c4a71eaf49ce464f0fb27b91
Dwane Dunn <Dwane.Dunn@gmail.com>
2016-07-19 17:36:05 -0400

    Part-1 Episode 9 add_action setup_child_theme

--- lib/setup.php
+++ lib/setup.php
@@ -11,48 +11,52 @@
 
 namespace KnowTheCode\Developers\Lib;
 
-//* Set Localization (do not remove)
-load_child_theme_textdomain( 'CHILD_TEXT_DOMAIN', apply_filters( 'child_theme_textdomain', get_stylesheet_directory() . '/languages', 'CHILD_TEXT_DOMAIN' ) );
+add_action( 'genesis_setup', __NAMESPACE__ . '\setup_child_theme' );
+function setup_child_theme() {
 
-//* Add HTML5 markup structure
-add_theme_support( 'html5', array( 'caption', 'comment-form', 'comment-list', 'gallery', 'search-form' ) );
+	//* Set Localization (do not remove)
+	load_child_theme_textdomain( 'CHILD_TEXT_DOMAIN', apply_filters( 'child_theme_textdomain', CHILD_THEME_DIR . '/languages', 'CHILD_TEXT_DOMAIN' ) );
 
-//* Add Accessibility support
-add_theme_support( 'genesis-accessibility', array(
-	'404-page',
-	'drop-down-menu',
-	'headings',
-	'rems',
-	'search-form',
-	'skip-links'
-) );
+	//* Add HTML5 markup structure
+	add_theme_support( 'html5', array( 'caption', 'comment-form', 'comment-list', 'gallery', 'search-form' ) );
 
-//* Add viewport meta tag for mobile browsers
-add_theme_support( 'genesis-responsive-viewport' );
+	//* Add Accessibility support
+	add_theme_support( 'genesis-accessibility', array(
+		'404-page',
+		'drop-down-menu',
+		'headings',
+		'rems',
+		'search-form',
+		'skip-links'
+	) );
 
-//* Add support for custom header
-add_theme_support( 'custom-header', array(
-	'width'           => 600,
-	'height'          => 160,
-	'header-selector' => '.site-title a',
-	'header-text'     => false,
-	'flex-height'     => true,
-) );
+	//* Add viewport meta tag for mobile browsers
+	add_theme_support( 'genesis-responsive-viewport' );
 
-//* Add support for custom background
-add_theme_support( 'custom-background' );
+	//* Add support for custom header
+	add_theme_support( 'custom-header', array(
+		'width'           => 600,
+		'height'          => 160,
+		'header-selector' => '.site-title a',
+		'header-text'     => false,
+		'flex-height'     => true,
+	) );
 
-//* Add support for after entry widget
-add_theme_support( 'genesis-after-entry-widget-area' );
+	//* Add support for custom background
+	add_theme_support( 'custom-background' );
 
-//* Add support for 3-column footer widgets
-add_theme_support( 'genesis-footer-widgets', 3 );
+	//* Add support for after entry widget
+	add_theme_support( 'genesis-after-entry-widget-area' );
 
-//* Rename primary and secondary navigation menus
-add_theme_support( 'genesis-menus', array(
-	'primary'   => __( 'After Header Menu', 'CHILD_TEXT_DOMAIN' ),
-	'secondary' => __( 'Footer Menu', 'CHILD_TEXT_DOMAIN' )
-) );
+	//* Add support for 3-column footer widgets
+	add_theme_support( 'genesis-footer-widgets', 3 );
 
-//* Add Image Sizes
-add_image_size( 'featured-image', 720, 400, true );
+	//* Rename primary and secondary navigation menus
+	add_theme_support( 'genesis-menus', array(
+		'primary'   => __( 'After Header Menu', 'CHILD_TEXT_DOMAIN' ),
+		'secondary' => __( 'Footer Menu', 'CHILD_TEXT_DOMAIN' )
+	) );
+
+	//* Add Image Sizes
+	add_image_size( 'featured-image', 720, 400, true );
+}
