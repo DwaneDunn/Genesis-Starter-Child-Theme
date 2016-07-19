6e9dd05f7374f0cabbc3d1357e30bf5238e36889
Dwane Dunn <Dwane.Dunn@gmail.com>
2016-07-19 18:21:49 -0400

    Part-1 Episode 10 Refactored with single config and arrays with a single foreach

--- lib/setup.php
+++ lib/setup.php
@@ -37,45 +37,43 @@ function setup_child_theme() {
  */
 function adds_theme_supports() {
 
-	//* Add HTML5 markup structure
-	add_theme_support( 'html5', array( 'caption', 'comment-form', 'comment-list', 'gallery', 'search-form' ) );
+	$config = array(
+		'html5' => array( 'caption', 'comment-form', 'comment-list', 'gallery', 'search-form' ),
+		'genesis-accessibility' => array(
+			'404-page',
+			'drop-down-menu',
+			'headings',
+			'rems',
+			'search-form',
+			'skip-links'
+		),
+		'genesis-responsive-viewport' => 'null',
+		'custom-header' => array(
+			'width'           => 600,
+			'height'          => 160,
+			'header-selector' => '.site-title a',
+			'header-text'     => false,
+			'flex-height'     => true,
+		),
+		'custom-header' =>array(
+			'width'           => 600,
+			'height'          => 160,
+			'header-selector' => '.site-title a',
+			'header-text'     => false,
+			'flex-height'     => true,
+		),
+		'custom-background' => 'null',
+		'genesis-after-entry-widget-area' => 'null',
+		'genesis-footer-widgets' => 3,
+		'genesis-menus' => array(
+			'primary'   => __( 'After Header Menu', 'CHILD_TEXT_DOMAIN' ),
+			'secondary' => __( 'Footer Menu', 'CHILD_TEXT_DOMAIN' )
+		),
+	);
 
-	//* Add Accessibility support
-	add_theme_support( 'genesis-accessibility', array(
-		'404-page',
-		'drop-down-menu',
-		'headings',
-		'rems',
-		'search-form',
-		'skip-links'
-	) );
-
-	//* Add viewport meta tag for mobile browsers
-	add_theme_support( 'genesis-responsive-viewport' );
-
-	//* Add support for custom header
-	add_theme_support( 'custom-header', array(
-		'width'           => 600,
-		'height'          => 160,
-		'header-selector' => '.site-title a',
-		'header-text'     => false,
-		'flex-height'     => true,
-	) );
-
-	//* Add support for custom background
-	add_theme_support( 'custom-background' );
-
-	//* Add support for after entry widget
-	add_theme_support( 'genesis-after-entry-widget-area' );
-
-	//* Add support for 3-column footer widgets
-	add_theme_support( 'genesis-footer-widgets', 3 );
-
-	//* Rename primary and secondary navigation menus
-	add_theme_support( 'genesis-menus', array(
-		'primary'   => __( 'After Header Menu', 'CHILD_TEXT_DOMAIN' ),
-		'secondary' => __( 'Footer Menu', 'CHILD_TEXT_DOMAIN' )
-	) );
+	foreach( $config as $feature => $args ) {
+		add_theme_support( $feature, $args );
+	}
 
 }
 
