32c62d82229e485c4a4370e6a2af99e22404af3c
Dwane Dunn <Dwane.Dunn@gmail.com>
2016-07-20 09:19:16 -0400

    Part-1 Eposiode 16 Refactored Autoload

--- lib/functions/autoload.php
+++ lib/functions/autoload.php
@@ -11,11 +11,65 @@
 
 namespace KnowTheCode\Developers\Functions;
 
-//* Setup Theme
-include_once( get_stylesheet_directory() . '/lib/theme-defaults.php' );
+/**
+ * Load non admin files
+ *
+ * @since 1.0.0
+ *
+ * @return void
+ *
+ */
+function load_nonadmin_files() {
+	$filenames = array(
+		'setup.php',
+		'components/customizer/css-handler.php',
+		'components/customizer/helpers.php',
+		'functions/formatting.php',
+		'functions/load-assets.php',
+		'functions/markup.php',
+//		'structure/archive.php',
+		'structure/comments.php',
+//		'structure/footer.php',
+//		'structure/header.php',
+		'structure/menu.php',
+		'structure/post.php',
+//		'structure/sidebar.php',
+	);
 
-//* Add Image upload and Color select to WordPress Theme Customizer
-require_once( get_stylesheet_directory() . '/lib/customize.php' );
+	load_specified_files( $filenames );
+}
+add_action( 'admin_init', __NAMESPACE__ . '\load_admin_files' );
 
-//* Include Customizer CSS
-include_once( get_stylesheet_directory() . '/lib/output.php' );
+/**
+ * Load admin files
+ *
+ * @since 1.0.0
+ *
+ * @return void
+ *
+ */
+function load_admin_files() {
+	$filenames = array(
+		'components/customizer/customizer.php',
+	);
+
+	load_specified_files( $filenames );
+}
+
+/**
+ * Load each of the specified files
+ *
+ * @since 1.0.0
+ *
+ * @param array $filenames
+ * @param string $folder_root
+ */
+function load_specified_files( array $filenames, $folder_root = '' ) {
+	$folder_root = $folder_root ?: CHILD_THEME_DIR . '/lib/';
+
+	foreach( $filenames as $filename ) {
+		include( $folder_root . $filename );
+	}
+}
+
+load_nonadmin_files();
