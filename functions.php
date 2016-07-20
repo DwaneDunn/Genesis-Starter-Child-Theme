722da95bc87d821bae85e022c700504a5a3f6363
Dwane Dunn <Dwane.Dunn@gmail.com>
2016-07-19 20:30:38 -0400

    Part-1 Fixed Stylesheet, sget_template_directory bug still present

--- functions.php
+++ functions.php
@@ -13,7 +13,7 @@ namespace KnowTheCode\Developers;
 
 include_once( 'lib/init.php' );
 
+include_once( 'lib/functions/autoload.php' );
+
 //* Start the Genesis Framework.
 include_once( get_template_directory() . '/lib/init.php' );
-
-include_once( 'lib/functions/autoload.php' );
